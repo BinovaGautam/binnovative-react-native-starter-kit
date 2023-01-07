3e8320feadc5808f660a071c72ad2acb6a6858fd
Binova Gautam <binova1245@gmail.com>
2023-01-07 19:55:43 +0530

    newtemp

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,21 +1,3 @@
-const presets = ['module:metro-react-native-babel-preset'];
-const plugins = [];
-
-plugins.push(
-  [
-    'module-resolver',
-    {
-      root: ['./src'],
-      extensions: ['.js', '.json'],
-      alias: {
-        '@': './src',
-      },
-    },
-  ],
-  'react-native-reanimated/plugin',
-);
-
 module.exports = {
-  presets,
-  plugins,
+  presets: ['module:metro-react-native-babel-preset'],
 };
