ee39b2c47addca27b9d186081cfa40f55f59deb4
Binova Gautam <33010562+BinovaGautam@users.noreply.github.com>
2023-12-29 09:07:27 +0530

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -1,27 +1,27 @@
-import inquirer from 'inquirer';
+// import inquirer from 'inquirer';
 
-let templateDir;
-const templateDirs = [
-  './MyApp',
-  './NewApp',
-  './newArchBase'
-];
+// let templateDir;
+// const templateDirs = [
+//   './MyApp',
+//   './NewApp',
+//   './newArchBase'
+// ];
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'templateDir',
-      message: 'Select a template directory:',
-      choices: templateDirs
-    }
-  ])
-  .then(answers => {
-    const userSelectedTemplateDir = answers.templateDir;
-    console.log(`You selected: ${userSelectedTemplateDir}`);
-    templateDir = userSelectedTemplateDir;
-    // Use the userSelectedTemplateDir variable in your code here
-  });
+// inquirer
+//   .prompt([
+//     {
+//       type: 'list',
+//       name: 'templateDir',
+//       message: 'Select a template directory:',
+//       choices: templateDirs
+//     }
+//   ])
+//   .then(answers => {
+//     const userSelectedTemplateDir = answers.templateDir;
+//     console.log(`You selected: ${userSelectedTemplateDir}`);
+//     templateDir = userSelectedTemplateDir;
+//     // Use the userSelectedTemplateDir variable in your code here
+//   });
 
 const config = {
   // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
@@ -32,7 +32,7 @@ const config = {
   titlePlaceholder: "Hello App Display Name",
 
   // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
-  templateDir: templateDir,
+  templateDir: './newArchBase',
 
   // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
   postInitScript: "./script.js",
